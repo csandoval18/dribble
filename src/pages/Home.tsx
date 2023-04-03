a18e5624e5faa72788f017eb634614587df2d484
Christian Sandoval <christiansandoval1018@outlook.com>
2023-04-03 18:22:18 -0500

    added header and improved hero layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,12 @@
 import React from "react"
 import Hero from "../components/Home/Hero"
-import CardsGrid from "../components/CardsGrid"
+import CardsGrid from "../components/Home/CardsGrid"
 
 interface HomeProps {}
 
 const Home: React.FC<HomeProps> = () => {
 	return (
-		<div className='home'>
+		<div className='Home'>
 			<Hero />
 			<CardsGrid />
 		</div>
