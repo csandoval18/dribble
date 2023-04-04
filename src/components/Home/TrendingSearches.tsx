1f542e9aea410f6f3b70d5efbc009668b6fc5d1e
Christian Sandoval <christiansandoval1018@outlook.com>
2023-04-04 18:41:42 -0500

    progressed with trending searches component

--- src/components/Home/TrendingSearches.tsx
+++ src/components/Home/TrendingSearches.tsx
@@ -2,10 +2,14 @@ import React from "react"
 
 interface TrendingSearchesProps {}
 
-const TrendingSearches: React.FC<
-	TrendingSearchesProps
-> = () => {
-	return <div className></div>
+const TrendingSearches: React.FC<TrendingSearchesProps> = () => {
+	return (
+		<ul className='TrendingSearches'>
+			<li>
+				<p>Trending searches</p>
+			</li>
+		</ul>
+	)
 }
 
 export default TrendingSearches
