5ebbf70ee13fd58480f833504b1253c362d8e5a9
Christian Sandoval <christiansandoval1018@outlook.com>
2023-04-09 23:31:45 -0500

    displaying username in cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,13 +3,15 @@ import { CardProps } from "../utils/types"
 
 const Card: React.FC<CardProps> = (cardProps: CardProps) => {
 	return (
-		<li className='card'>
-			<figure>
-				<img src={cardProps.imageSrc} alt='' />
-			</figure>
-			<div className='details'>
-				<div className='user-info'></div>
-				<div className='stats'></div>
+		<li className='card-item'>
+			<div className='card'>
+				<figure>
+					<img src={cardProps.imageSrc} alt='' />
+				</figure>
+				<div className='details'>
+					<div className='user-info'>{cardProps.user}</div>
+					<div className='stats'></div>
+				</div>
 			</div>
 		</li>
 	)
